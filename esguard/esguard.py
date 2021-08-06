31526c53e8a9b1664f0a84bded246d97c2b6cea9
hirosassa <hiro.sassa@gmail.com>
2021-08-06 09:51:37 +0900

    use tenacity

--- esguard/esguard.py
+++ esguard/esguard.py
@@ -1,10 +1,9 @@
 from functools import wraps
-import time
-import random
-from typing import Callable, TypeVar, Any
-from elasticsearch import Elasticsearch
 from logging import Logger
+from typing import Callable, TypeVar, Any
 
+from elasticsearch import Elasticsearch
+from tenacity import Retrying, wait_exponential, wait_random, stop_after_attempt, RetryError
 
 T = TypeVar("T")
 
@@ -17,6 +16,10 @@ class MaxRetriesExceededError(Error):
     pass
 
 
+class ResourceUsageError(Error):
+    pass
+
+
 class ESGuard:
     def __init__(
         self,
@@ -24,6 +27,8 @@ class ESGuard:
         os_cpu_percent: int = 90,
         os_mem_used_percent: int = 90,
         jvm_mem_heap_used_percent: int = 90,
+        retry_backoff_sec: int = 1,
+        max_retries: int = 3,
         logger: Logger = None,
     ) -> None:
         self.logger = logger
@@ -31,60 +36,45 @@ class ESGuard:
         self.os_cpu_percent = os_cpu_percent
         self.os_mem_used_percent = os_mem_used_percent
         self.jvm_mem_heap_used_percent = jvm_mem_heap_used_percent
+        self.retry_backoff_sec = retry_backoff_sec
+        self.max_retries = max_retries
 
     def _warning(self, message: str) -> None:
         if self.logger is None:
             return
         self.logger.warning(message)
 
-    def _wait(self) -> bool:
-        wait = False
+    def _wait(self):
         res = self.es.nodes.stats(metric=["os", "jvm"])
         for k in res["nodes"].keys():
             cpu = res["nodes"][k]["os"]["cpu"]["percent"]
             if cpu >= self.os_cpu_percent and self.os_cpu_percent > 0:
-                self._warning(
-                    f"node({k}) OS CPU usage {cpu}% over {self.os_cpu_percent}% "
-                )
-                wait = True
-                break
+                self._warning(f"node({k}) OS CPU usage {cpu}% over {self.os_cpu_percent}% ")
+                raise ResourceUsageError(f"node({k}) OS CPU usage {cpu}% over {self.os_cpu_percent}% ")
 
             mem = res["nodes"][k]["os"]["mem"]["used_percent"]
             if mem >= self.os_mem_used_percent and self.os_mem_used_percent > 0:
-                self._warning(
-                    f"node({k}) OS MEM usage {mem}% over {self.os_mem_used_percent}% "
-                )
-                wait = True
-                break
+                self._warning(f"node({k}) OS MEM usage {mem}% over {self.os_mem_used_percent}% ")
+                raise ResourceUsageError(f"node({k}) OS MEM usage {mem}% over {self.os_mem_used_percent}% ")
 
             jvm_heap = res["nodes"][k]["jvm"]["mem"]["heap_used_percent"]
-            if (
-                jvm_heap >= self.jvm_mem_heap_used_percent
-                and self.jvm_mem_heap_used_percent > 0
-            ):
-                self._warning(
-                    f"node({k}) JVM heap usage {jvm_heap}% over {self.jvm_mem_heap_used_percent}% "
-                )
-                wait = True
-                break
+            if (jvm_heap >= self.jvm_mem_heap_used_percent and self.jvm_mem_heap_used_percent > 0):
+                self._warning(f"node({k}) JVM heap usage {jvm_heap}% over {self.jvm_mem_heap_used_percent}% ")
+                raise ResourceUsageError(f"node({k}) JVM heap usage {jvm_heap}% over {self.jvm_mem_heap_used_percent}% ")
 
-        return wait
+    def _get_retryer(self):
+        return Retrying(wait=wait_exponential(min=self.retry_backoff_sec) + wait_random(min=0, max=1), stop=stop_after_attempt(self.max_retries))
 
     def decotator(self) -> Callable[[], T]:
+        retryer = self._get_retryer()
+
         def _retry(func) -> Callable[[], T]:
             @wraps(func)
             def wrapper(*args, **kwargs) -> Any:
-                x = 0
-                backoff_in_seconds = 1
-                retries = 3
-
-                while self._wait():
-                    if x == retries:
-                        raise MaxRetriesExceededError(f"max retries exceeded {retries}")
-                    sleep = backoff_in_seconds * 2 ** x + random.uniform(0, 1)
-                    time.sleep(sleep)
-                    x += 1
-
+                try:
+                    retryer(self._wait)
+                except RetryError:
+                    raise MaxRetriesExceededError(f"max retries exceeded {self.max_retries}")
                 return func(*args, **kwargs)
 
             return wrapper
